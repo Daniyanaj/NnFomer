58526ff52c914e8a96e3ae67ef68bf1b714406ab
Daniyanaj <mdan5181@gmail.com>
2022-10-09 04:05:11 +0400

    mistake_removed

--- nnformer/network_architecture/nnFormer_synapse.py
+++ nnformer/network_architecture/nnFormer_synapse.py
@@ -391,7 +391,7 @@ class SwinTransformerBlock(nn.Module):
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.mlp = Mlp_seg(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
         #self.grid_attn = grid_attn
        
     def forward(self, x, mask_matrix):
