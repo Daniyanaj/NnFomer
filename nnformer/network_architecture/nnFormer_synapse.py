63dc0bde4513f35379c88255d15e13488bd6368b
Daniyanaj <mdan5181@gmail.com>
2022-10-09 04:43:56 +0400

    initial

--- nnformer/network_architecture/nnFormer_synapse.py
+++ nnformer/network_architecture/nnFormer_synapse.py
@@ -40,20 +40,20 @@ class Mlp_seg(nn.Module):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
-        #self.fc1 = nn.Conv3d(in_features, hidden_features, 1)
+        self.fc1 = nn.Conv3d(in_features, hidden_features, 1)
         self.dwconv = DWConv(in_features)
         self.act = act_layer()
         self.fc2 = nn.Conv3d(hidden_features, out_features, 1)
         self.drop = nn.Dropout(drop) 
     
     def forward(self, x):
-        #x = self.fc1(x)
+        x = self.fc1(x)
         #x=x.reshape(3072,1,1)
-        x = self.dwconv(x)
+        #x = self.dwconv(x)
         x = self.act(x)
         x = self.drop(x)
-        #x = self.fc2(x)
-        #x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
 
         return x
     
