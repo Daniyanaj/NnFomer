fd6f7d84d53e1a9504ed2436d0f6abce3f292510
Daniyanaj <mdan5181@gmail.com>
2022-10-11 23:26:32 +0400

    sma

--- train_inference.sh
+++ train_inference.sh
@@ -18,7 +18,7 @@ if ${train}
 then
 	
 	cd /home/daniya.kareem/nnFormer/nnformer/
-	CUDA_VISIBLE_DEVICES=${cuda} nnFormer_train 3d_fullres nnFormerTrainerV2_${name} ${task} 0
+	CUDA_VISIBLE_DEVICES=${cuda} nnFormer_train 3d_fullres nnFormerTrainerV2_${name} ${task} 0 -c
 fi
 
 
