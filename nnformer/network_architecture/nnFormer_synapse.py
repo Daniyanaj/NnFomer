b01c68ac6eb0794e034f9395c1f946a6fe336c37
Daniyanaj <mdan5181@gmail.com>
2022-10-21 18:15:03 +0400

    init

--- nnformer/network_architecture/nnFormer_synapse.py
+++ nnformer/network_architecture/nnFormer_synapse.py
@@ -571,21 +571,8 @@ class BasicLayer(nn.Module):
         # build blocks
         
         self.blk = nn.ModuleList()
-        for i in range(2):
-            self.blk.append(
-                SwinTransformerBlock(
-                dim=dim,
-                input_resolution=input_resolution,
-                num_heads=num_heads,
-                window_size=window_size,
-                shift_size=0 if (i % 2 == 0) else window_size // 2,
-                mlp_ratio=mlp_ratio,
-                qkv_bias=qkv_bias,
-                qk_scale=qk_scale,
-                drop=drop,
-                attn_drop=attn_drop,
-                drop_path=drop_path[i] if isinstance(drop_path, list) else drop_path, norm_layer=norm_layer)
-            )
+        
+            
             
         for j in range(2):
             self.blk.append(
@@ -631,11 +618,10 @@ class BasicLayer(nn.Module):
                                          self.window_size * self.window_size * self.window_size)  
         attn_mask = mask_windows.unsqueeze(1) - mask_windows.unsqueeze(2)
         attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))
-        x = self.blk[0](x,attn_mask)
-        x = self.blk[1](x,attn_mask)
-        x = self.blk[2](x,i,S,H)
         
-        x = self.blk[3](x,i,S,H)
+        x = self.blk[0](x,i,S,H)
+        
+        x = self.blk[1](x,i,S,H)
             
         if self.downsample is not None:
             x_down = self.downsample(x, S, H, W)
@@ -670,33 +656,9 @@ class BasicLayer_up(nn.Module):
         # build blocks
       
         self.blocks = nn.ModuleList()
-        self.blocks.append(
-                SwinTransformerBlock_kv(
-                    dim=dim,
-                    input_resolution=input_resolution,
-                    num_heads=num_heads,
-                    window_size=window_size,
-                    shift_size=0 ,
-                    mlp_ratio=mlp_ratio,
-                    qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
-                    drop=drop,
-                    attn_drop=attn_drop,
-                    drop_path=drop_path[0] if isinstance(drop_path, list) else drop_path, norm_layer=norm_layer)
-                    )
-        self.blocks.append( 
-                SwinTransformerBlock(
-                    dim=dim,
-                    input_resolution=input_resolution,
-                    num_heads=num_heads,
-                    window_size=window_size,
-                    shift_size=0 if (1 % 2 == 0) else window_size // 2,
-                    mlp_ratio=mlp_ratio,
-                    qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
-                    drop=drop,
-                    attn_drop=attn_drop,
-                    drop_path=drop_path[1] if isinstance(drop_path, list) else drop_path, norm_layer=norm_layer))
+      
+                    
+       
         for i in range(2):    
             self.blocks.append(
                     GNN_Transformer(
@@ -744,13 +706,12 @@ class BasicLayer_up(nn.Module):
         
         # for i in range(self.depth):
         #     x = self.blocks(x, S,H,W)
-        x = self.blocks[0](x, attn_mask, skip=skip, x_up=x_up)
-        x= self.blocks[1](x, attn_mask)
-        x= self.blocks[2](x,i,S,H)
+        
+        x= self.blocks[0](x,i,S,H)
 
 
         
-        x = self.blocks[3](x, i,S ,H)
+        x = self.blocks[1](x, i,S ,H)
             
         return x, S, H, W
         
