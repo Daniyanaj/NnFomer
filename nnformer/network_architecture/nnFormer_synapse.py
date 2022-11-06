f4e58e241e874a236984ed463e7f93f9272c6d0e
Daniyanaj <mdan5181@gmail.com>
2022-11-07 00:54:38 +0400

    dan

--- nnformer/network_architecture/nnFormer_synapse.py
+++ nnformer/network_architecture/nnFormer_synapse.py
@@ -13,6 +13,43 @@ import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 from timm.models.layers import DropPath, to_3tuple, trunc_normal_
 
+class DWConv(nn.Module):
+    def __init__(self, dim=768):
+        super(DWConv, self).__init__()
+        self.dwconv = nn.Conv3d(dim, dim, 3, 1, 1, bias=True, groups=dim)
+
+    def forward(self, x):
+        x = self.dwconv(x)
+        x = x.flatten(2).transpose(1, 2)
+        return x
+
+class DWMlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0., linear=False):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.dwconv = DWConv(hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+
+
+
+    def forward(self, x):
+        x = self.fc1(x)
+        B, N, C = x.shape
+        H = W = S = int(round((N) ** (1 / 3)))
+        x = x.view(B, C, H, W,S)
+        x = self.dwconv(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
 class ContiguousGrad(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x):
@@ -841,7 +878,7 @@ class Encoder2(nn.Module):
    
     def __init__(self,
                  pretrain_img_size=224,
-                 patch_size=16,
+                 patch_size=4,
                  in_chans=1  ,
                  embed_dim=96,
                  depths=[2, 2, 2, 2],
@@ -903,6 +940,11 @@ class Encoder2(nn.Module):
 
         num_features = [int(embed_dim * 2 ** i) for i in range(self.num_layers)]
         self.num_features = num_features
+        in_features=int(1536)
+
+        act_layer=nn.GELU
+        drop=0
+        self.mlp=DWMlp(in_features, hidden_features=in_features*4, act_layer=act_layer, drop=drop)
 
         # add a norm layer for each output
         for i_layer in out_indices:
@@ -924,13 +966,13 @@ class Encoder2(nn.Module):
         
       
         for i in range(1):
-            layer = self.layers[i]
-            x_out, S, H, W, x, Ws, Wh, Ww = layer(x, Ws, Wh, Ww)
+            
+            x_out=self.mlp(x)
             if i in self.out_indices:
                 norm_layer = getattr(self, f'norm{3}')
-                x_out = norm_layer(x_out)
+                out = norm_layer(x_out)
 
-                out = x_out.view(-1, S, H, W, self.num_features[3]).permute(0, 4, 1, 2, 3).contiguous()
+                out = x_out.view(-1, 2,2, 2, self.num_features[3]).permute(0, 4, 1, 2, 3).contiguous()
               
                 down.append(out)
         return down
