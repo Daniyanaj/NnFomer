b9ea845670b92d844458ffba83305d73fdcc635e
Daniyanaj <mdan5181@gmail.com>
2022-11-21 19:49:30 +0400

    init

--- nnformer/training/network_training/network_trainer_synapse.py
+++ nnformer/training/network_training/network_trainer_synapse.py
@@ -119,7 +119,8 @@ class NetworkTrainer(object):
             self.use_progress_bar = bool(int(os.environ['nnformer_use_progress_bar']))
 
         ################# Settings for saving checkpoints ##################################
-        self.save_every = 100
+        if self.epoch>750:
+            self.save_every = 30
         self.save_latest_only = False  # if false it will not store/overwrite _latest but separate files each
         # time an intermediate checkpoint is created
         self.save_intermediate_checkpoints = True  # whether or not to save checkpoint_latest
