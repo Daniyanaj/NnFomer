9f2c47b1252888e174f4210233b61c297246e6f5
Daniyanaj <mdan5181@gmail.com>
2022-11-22 23:06:57 +0400

    a

--- nnformer/network_architecture/nnFormer_synapse.py
+++ nnformer/network_architecture/nnFormer_synapse.py
@@ -7,11 +7,117 @@ import numpy as np
 from nnformer.network_architecture.initialization import InitWeights_He
 from nnformer.network_architecture.neural_network import SegmentationNetwork
 import torch.nn.functional
+from einops.layers.torch import Reduce,Rearrange
 
 
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 from timm.models.layers import DropPath, to_3tuple, trunc_normal_
+
+##
+
+# helper classes
+
+class PreNormResidual(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(self.norm(x)) + x
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult = 4, dropout = 0.):
+        super().__init__()
+        inner_dim = int(dim * mult)
+        self.net = nn.Sequential(
+            nn.Linear(dim, inner_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+# MBConv
+
+class SqueezeExcitation(nn.Module):
+    def __init__(self, dim, shrinkage_rate = 0.25):
+        super().__init__()
+        hidden_dim = int(dim * shrinkage_rate)
+
+        self.gate = nn.Sequential(
+            Reduce('b c h w s -> b c', 'mean'),
+            nn.Linear(dim, hidden_dim, bias = False),
+            nn.SiLU(),
+            nn.Linear(hidden_dim, dim, bias = False),
+            nn.Sigmoid(),
+            Rearrange('b c -> b c 1 1 1' )
+        )
+
+    def forward(self, x):
+        return x * self.gate(x)
+
+
+class MBConvResidual(nn.Module):
+    def __init__(self, fn, dropout = 0.):
+        super().__init__()
+        self.fn = fn
+        self.dropsample = Dropsample(dropout)
+
+    def forward(self, x):
+        l,n,c=x.shape
+        h=w=s= int(round((n)**(1/3)))
+        x=x.view(l,c,h,w,s)
+        out = self.fn(x)
+        out = self.dropsample(out)
+        return out + x
+
+class Dropsample(nn.Module):
+    def __init__(self, prob = 0):
+        super().__init__()
+        self.prob = prob
+  
+    def forward(self, x):
+        device = x.device
+
+        if self.prob == 0. or (not self.training):
+            return x
+
+        keep_mask = torch.FloatTensor((x.shape[0], 1, 1, 1,1), device = device).uniform_() > self.prob
+        return x * keep_mask / (1 - self.prob)
+
+def MBConv(
+    dim_in,
+    dim_out,
+    *,
+    downsample,
+    expansion_rate = 4,
+    shrinkage_rate = 0.25,
+    dropout = 0.
+):
+    hidden_dim = int(expansion_rate * dim_out)
+    stride = 2 if downsample else 1
+
+    net = nn.Sequential(
+        nn.Conv3d(dim_in, hidden_dim, 1),
+        nn.BatchNorm3d(hidden_dim),
+        nn.GELU(),
+        nn.Conv3d(hidden_dim, hidden_dim, 3, stride = stride, padding = 1, groups = hidden_dim),
+        nn.BatchNorm3d(hidden_dim),
+        nn.GELU(),
+        SqueezeExcitation(hidden_dim, shrinkage_rate = shrinkage_rate),
+        nn.Conv3d(hidden_dim, dim_out, 1),
+        nn.BatchNorm3d(dim_out)
+    )
+
+    if dim_in == dim_out and not downsample:
+        net = MBConvResidual(net, dropout = dropout)
+        #x=net(x)
+    return net
+
 
 class ContiguousGrad(torch.autograd.Function):
     @staticmethod
@@ -489,10 +595,19 @@ class BasicLayer(nn.Module):
             self.downsample = downsample(dim=dim, norm_layer=norm_layer)
         else:
             self.downsample = None
+        self.mb=  MBConv(
+                        dim,
+                        dim,
+                        downsample = 0,
+                        expansion_rate = 4,
+                        shrinkage_rate = 0.25,
+                    )
 
     def forward(self, x, S, H, W):
       
-
+        x=self.mb(x)
+        b,c,l,m,n=x.shape
+        x=x.view(b,l*m*n,c)
         # calculate attention mask for SW-MSA
         Sp = int(np.ceil(S / self.window_size)) * self.window_size
         Hp = int(np.ceil(H / self.window_size)) * self.window_size
@@ -587,12 +702,22 @@ class BasicLayer_up(nn.Module):
 
         
         self.Upsample = upsample(dim=2*dim, norm_layer=norm_layer)
+        self.mb=  MBConv(
+                        dim,
+                        dim,
+                        downsample = 0,
+                        expansion_rate = 4,
+                        shrinkage_rate = 0.25,
+                    )
     def forward(self, x,skip, S, H, W):
         
       
         x_up = self.Upsample(x, S, H, W)
        
         x = x_up + skip
+        x=self.mb(x)
+        b,c,l,m,n=x.shape
+        x=x.view(b,l*m*n,c)
         S, H, W = S * 2, H * 2, W * 2
         # calculate attention mask for SW-MSA
         Sp = int(np.ceil(S / self.window_size)) * self.window_size
