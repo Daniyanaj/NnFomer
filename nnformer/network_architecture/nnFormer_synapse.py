5565fac75fdaea470d467c7021637ca059b30eef
Daniyanaj <mdan5181@gmail.com>
2022-11-27 22:14:03 +0400

    a

--- nnformer/network_architecture/nnFormer_synapse.py
+++ nnformer/network_architecture/nnFormer_synapse.py
@@ -417,6 +417,7 @@ class SwinTransformerBlock(nn.Module):
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.conv=nn.Conv3d(dim+4,dim ,1)
         
        
     def forward(self, x, mask_matrix):
@@ -453,7 +454,7 @@ class SwinTransformerBlock(nn.Module):
         #                            C)  
         # d,e,f=x_windows.shape 
         x_windows=x_windows.view(B,C, H, W ,S)
-        shift_size=1
+        shift_size=5
         pad = shift_size//2
 
         x = F.pad(x_windows, (pad,pad, pad, pad, pad,pad,pad,pad) , "constant", 0)
@@ -463,9 +464,11 @@ class SwinTransformerBlock(nn.Module):
         x_cat = torch.narrow(x_cat, 2, pad, H)
         x_cat = torch.narrow(x_cat, 3, pad, W)
         x_windows= torch.narrow(x_cat, 4, pad, S)
+        x_windows=self.conv(x_windows)
         
         x_windows= x_windows.view(-1, self.window_size * self.window_size * self.window_size,
                                   C)  
+                                  
                                 
         attn_windows = self.attn(x_windows, mask=attn_mask,pos_embed=None)  
 
